7adca6f5118a1f44e1f9e244b7891c33c9dcdd32
tnrao-trainer <40280456+tnrao-trainer@users.noreply.github.com>
2025-11-03 11:09:00 +0530

    Add files via upload

---
+++ 04.Week-4/15.Day15_C#_Fundamentals/Session Examples/Eg1_Program_Basic.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string uname = "Scott";
+            int age = 20;
+            bool isValidUser = true;
+
+
+            Console.WriteLine("------  User Details ----------");
+            Console.WriteLine("User Name  : " + uname);
+            Console.WriteLine("User Age  : " + age);
+            Console.WriteLine("Is Valid User : " + isValidUser);
+          
+
+
+            Console.ReadLine(); 
+        }
+    }
+}
---
+++ 04.Week-4/15.Day15_C#_Fundamentals/Session Examples/Eg2_Program_UserInfo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Console.WriteLine("Enter User Name :  ");
+            string uname = Console.ReadLine();
+
+            Console.WriteLine("Enter User Age :  ");
+            int age =  int.Parse( Console.ReadLine() );
+
+            Console.WriteLine("Is Valid User (true/false) :  ");
+            bool isValidUser = bool.Parse(Console.ReadLine());     
+            
+
+            Console.WriteLine("------  User Details ----------");
+            Console.WriteLine("User Name  : " + uname);
+            Console.WriteLine("User Age  : " + age);
+            Console.WriteLine("Is Valid User : " + isValidUser);
+          
+
+
+            Console.ReadLine(); 
+        }
+    }
+}
