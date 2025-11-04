2a0d802ab94cc07baf5d45646b40741d775e49ed
tnrao-trainer <40280456+tnrao-trainer@users.noreply.github.com>
2025-11-04 10:43:42 +0530

    Add files via upload

---
+++ 04.Week-4/16.Day16_C#_Fundamentals_Methods/Session_Examples/Eg1_Methods_Parameters_Basics.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ConsoleApp38
+{
+    internal class Program
+    { 
+        // 1.  Call By Value
+        static void Test1(int x)
+        {
+            x = x + 10;
+        }
+
+        // 2.  Call By Ref
+        static void Test2(ref int x)
+        {
+            x = x + 10;
+        }
+
+        // 3.Out Parameters
+        static  void  FindTotal(int x, int y, out int z)
+        {
+            z = x + y;
+        }
+
+        // 4. Default Parameters
+        static int  GetTotalAmount(int price, int qty = 1)
+        {
+            return price * qty;
+        }
+
+
+        // 5. Unlimited Parameters
+        static void Sum(params int[] x)
+        {
+            int s = 0;
+            foreach (int item in x)
+            {
+                s += item;
+            }
+            Console.WriteLine($"Sum Result : {s}");
+        }
+
+        static void Main(string[] args)
+        {
+            int a = 10;
+
+            Console.WriteLine($"Before : {a}");
+            Test1(a);
+            Console.WriteLine($"After : {a}");
+
+            Console.WriteLine("---------------------------------");
+
+            Console.WriteLine($"Before : {a}");
+            Test2(ref a);
+            Console.WriteLine($"After : {a}");
+
+            Console.WriteLine("-----------------------------");
+            //---------------------------
+            int p = 10, q = 20, r;
+            FindTotal(p, q, out r);
+            Console.WriteLine($"Total Result : {r}");
+
+            Console.WriteLine("--------------------------");
+
+            Console.WriteLine("Total Amount :  " + GetTotalAmount(2500, 3));
+            Console.WriteLine("Total Amount :  " + GetTotalAmount(1500));
+
+            Console.WriteLine("--------------------------");
+            Sum(10);
+            Sum(10, 20);
+            Sum(10, 20, 30);
+            Sum(10, 20, 30, 40);
+
+            Console.ReadLine();   
+        }
+
+
+    }
+}
---
+++ 04.Week-4/16.Day16_C#_Fundamentals_Methods/Session_Examples/Eg3_Method_Overloaindg_Program.cs
@@ -0,0 +1,42 @@
+﻿
+
+namespace ConsoleApp39
+{
+    internal class Program
+    {
+        static void Greeting()
+        {
+            Console.WriteLine("Welcome to C# Programming");
+        }
+
+        static void Greeting(string name)
+        {
+            Console.WriteLine($"Hi {name}, Welcome to C# Programming");
+        }
+
+        static void Greeting(string name, int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine($"Hi {name}, Welcome to C# Programming");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+          
+            Greeting();
+            Greeting();
+            Console.WriteLine("-----------------------");
+
+            Greeting("Narasimha");
+            Greeting("Scott");
+            Console.WriteLine("-------------------------");
+
+            Greeting("Stephen", 3);
+            
+
+            Console.ReadLine();
+        }
+    }
+}
