5c0f61011c5edb87683c6dbcccbdc44ff4952c07
tnrao-trainer <40280456+tnrao-trainer@users.noreply.github.com>
2025-11-04 13:06:02 +0530

    Add files via upload

---
+++ 04.Week-4/16.Day16_C#_Fundamentals_Methods/Session_Examples/Eg5_Program_ThrowingExceptions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp39
+{
+    class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                Console.Write("Enter your age: ");
+                int age = Convert.ToInt32(Console.ReadLine());
+
+                if (age < 0 || age > 100)
+                {
+                    throw new Exception("Age should not be less than zero or greater than 100.");
+                }
+
+                Console.WriteLine("Your age is valid");
+
+              
+            } 
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception Message: {ex.Message}.");
+                Console.WriteLine($"Stack Trace  :  {ex.StackTrace}");
+            }
+            
+
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
