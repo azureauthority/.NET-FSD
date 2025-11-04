c5648a4943d3ab4a8444f2963409390d8d852f8d
tnrao-trainer <40280456+tnrao-trainer@users.noreply.github.com>
2025-11-04 17:27:31 +0530

    Add files via upload

---
+++ 04.Week-4/16.Day16_C#_Fundamentals_Methods/Session_Examples/Eg7_Program_Strings.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string s1, s2;
+        s1 = "Hello";
+        s2 = " World";
+
+
+        s1 = string.Concat(s1, s2);
+        Console.WriteLine("Concate Result : {0}", s1);
+
+        Console.WriteLine("Upper:  {0}", s1.ToUpper());		
+        Console.WriteLine("Lower:  {0}", s1.ToLower());
+
+
+       bool flag = s1.Contains("World");
+       Console.WriteLine("Contains : {0}", flag);
+
+
+        int n = s1.IndexOf("llo");
+        Console.WriteLine("IndexOf : {0}", n);
+
+        n = s1.LastIndexOf('l');
+       Console.WriteLine("LastIndexOf : {0}", n);
+
+        Console.WriteLine("Substring : {0}", s1.Substring(4));
+        Console.WriteLine("Substring : {0}", s1.Substring(4, 3));
+
+        Console.WriteLine();
+        string s3 = "       ABC APP          ";       
+	   Console.WriteLine("With spaces : {0}", s3.Length);
+        s3 = s3.Trim();
+        Console.WriteLine("After trim : {0}", s3.Length);
+
+        Console.WriteLine();
+        string s4 = "ASP.NET,VB.NET,C#.NET";
+        string[] strArray =  s4.Split(',');
+        foreach (string str in strArray)
+        {
+            Console.Write("  " + str);
+        }
+    }
+
+}
---
+++ 04.Week-4/16.Day16_C#_Fundamentals_Methods/Session_Examples/Eg8_Program_Strings_StringBuilder.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        StringBuilder sb = new StringBuilder();
+
+        Console.WriteLine("[Using StringBuilder]");
+
+        Console.WriteLine("Start Time: {0}", DateTime.Now.ToString("T"));
+
+        for (int i = 0; i < 100000; i++)
+        {
+            sb.Append(i);
+        }
+
+        Console.WriteLine("End Time: {0}", DateTime.Now.ToString("T"));
+
+        Console.WriteLine();
+
+        string str = "";
+        Console.WriteLine("[Using String]");
+        Console.WriteLine("Start Time: {0}", DateTime.Now.ToString("T"));
+
+        for (int i = 0; i < 100000; i++)
+        {
+            str = str + i;
+        }
+        Console.WriteLine("End Time: {0}", DateTime.Now.ToString("T"));
+
+        Console.WriteLine();
+    }
+
+}
