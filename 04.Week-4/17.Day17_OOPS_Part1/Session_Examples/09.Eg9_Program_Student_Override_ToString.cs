d1f95030575cccb960bf1730af7f9403c0bd0f15
tnrao-trainer <40280456+tnrao-trainer@users.noreply.github.com>
2025-11-06 17:07:50 +0530

    Add files via upload

---
+++ 04.Week-4/17.Day17_OOPS_Part1/Session_Examples/09.Eg9_Program_Student_Override_ToString.cs
@@ -0,0 +1,59 @@
+﻿
+namespace ConsoleApp39
+{
+    class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Course { get; set; }
+        public string Email {  get; set; }
+
+
+        public override string ToString()
+        {
+            return $"{Id}, {Name}, {Course}, {Email}";
+        }
+
+    }
+
+    class Test
+    {
+
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Test  t = new Test();
+            Console.WriteLine(t.ToString() );
+            Console.WriteLine("-----------------------------");
+
+            Student student = new Student();
+
+            student.Id = 1256;
+            student.Name = "Scott";
+            student.Course = "C#.NET";
+            student.Email = "scott@gmail.com";
+
+
+            Console.WriteLine(student);
+            Console.WriteLine(student.ToString() );
+
+            Console.WriteLine("-------------------------------");
+
+
+            List<int> lstObj = new List<int>();
+            Console.WriteLine(lstObj );
+            Console.WriteLine(lstObj.ToString() );
+            Console.WriteLine("------------------------------------------");
+
+            String s1 = new string("Hello");
+            Console.WriteLine(s1);
+            Console.WriteLine(s1.ToString() );
+ 
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 04.Week-4/17.Day17_OOPS_Part1/Session_Examples/10.Eg10_Program_Runtime_Polymorphisism.cs
@@ -0,0 +1,64 @@
+﻿
+namespace ConsoleApp39
+{
+    class Person
+    {
+        public virtual void Print()
+        {
+            Console.WriteLine("Person details from parent class.");
+        }
+    }
+    class Employee : Person
+    {
+        public override void Print()
+        {
+            
+            Console.WriteLine("Employee details from child class");
+        }
+    }
+
+    class Student : Person
+    {
+        public override void Print()
+        {
+            Console.WriteLine("Student details from child class");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person;
+
+            person = new Person();
+            person.Print();
+
+            person = new Employee();
+            person.Print();
+
+            person = new Student();
+            person.Print();
+            Console.WriteLine("---------------------------------");
+
+
+
+            PrintInfo(new Person());
+            PrintInfo(new Employee());
+            PrintInfo(new Student());
+
+           
+            Console.ReadLine();
+        }
+    
+    
+    
+        static void  PrintInfo(Person person)
+        {
+            person.Print();
+        }
+
+
+
+    }
+}
