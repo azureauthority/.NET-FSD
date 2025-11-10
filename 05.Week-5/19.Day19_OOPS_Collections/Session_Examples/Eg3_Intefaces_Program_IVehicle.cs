75760fbc1e42ab616060fc6c9880a285a488576c
tnrao-trainer <40280456+tnrao-trainer@users.noreply.github.com>
2025-11-10 12:59:06 +0530

    Add files via upload

---
+++ 05.Week-5/19.Day19_OOPS_Collections/Session_Examples/Eg3_Intefaces_Program_IVehicle.cs
@@ -0,0 +1,48 @@
+﻿
+
+namespace ConsoleApp39
+{
+
+    interface IVehicle
+    {
+        void SpeedUp(int increment);
+        void CheckSpeed();
+        int Speed { get; set; } // Property
+    }
+
+
+    class Bicycle : IVehicle
+    {
+        public int Speed { get; set; }
+
+        public void SpeedUp(int increment)
+        {
+            Speed += increment;
+        }
+
+        public void CheckSpeed()
+        {
+            Console.WriteLine($"Current speed: {Speed}");
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Bicycle b = new Bicycle();
+            b.Speed = 100;
+            b.CheckSpeed();
+
+            b.SpeedUp(20);
+            b.CheckSpeed();
+
+            b.SpeedUp(20);
+            b.CheckSpeed();
+
+            Console.ReadLine();
+        }
+    }
+}
