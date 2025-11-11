e5e0e3b0195fcc9500a64bdc50f284201b9e3542
tnrao-trainer <40280456+tnrao-trainer@users.noreply.github.com>
2025-11-11 13:26:24 +0530

    Add files via upload

---
+++ 05.Week-5/20.Day20_ASP.NET_Core_WebAPI/Session_Examples/01.Eg1_WebAPI_Employee_Basic/Controllers/EmployeesController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication10.Models;
+
+namespace WebApplication10.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+
+            List<Employee> empList = new List<Employee>()
+            {
+                new Employee() { Id = 1, Name = "Alice", Job = "Manager", Salary = 75000, Deptno = 10 },
+                new Employee() { Id = 2, Name = "Bob", Job = "Developer", Salary = 65000, Deptno = 20 },
+                new Employee() { Id = 3, Name = "Charlie", Job = "Analyst", Salary = 60000, Deptno = 20 }
+            };
+
+            return Ok(empList);
+        }
+
+
+        [HttpGet("{id}")]
+        public IActionResult GetEmployeeById(int id)
+        {
+            List<Employee> empList = new List<Employee>()
+            {
+                new Employee() { Id = 1, Name = "Alice", Job = "Manager", Salary = 75000, Deptno = 10 },
+                new Employee() { Id = 2, Name = "Bob", Job = "Developer", Salary = 65000, Deptno = 20 },
+                new Employee() { Id = 3, Name = "Charlie", Job = "Analyst", Salary = 60000, Deptno = 20 }
+            };
+
+            Employee obj = empList.FirstOrDefault( item => item.Id == id ) ;
+
+            if (obj != null)
+                return Ok(obj);
+            else
+                return NotFound(new { status = "Failed", message = "Requested employee id does not exists" });
+        }
+
+    }
+}
